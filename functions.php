d872725c0e32c9465ddc69f044aafd8abaf41dbc
ControlledChaos <greg@ccdzine.com>
2020-11-26 21:14:48 -0800

    New directories. Blank functions file.

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Courtney Theme core functions file
+ *
+ * Designed and built for the Courtney Hoffman Costume Designer website.
+ *
+ * @package    WordPress/ClassicPress
+ * @subpackage Courtney_Theme
+ * @author     Controlled Chaos Design <greg@ccdzine.com>
+ * @copyright  Copyright (c) Controlled Chaos Design
+ * @link       https://github.com/ControlledChaos/chcd-theme
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ * @since      1.0.0
+ */
